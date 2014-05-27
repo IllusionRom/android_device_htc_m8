5e9ff30c4fb6e26bcbdcd32a2761c09547cd8551
Jake Weinstein <xboxlover360@gmail.com>
2014-05-27 10:39:29 -0500

    m8: use common_phone mk so we have APNs

--- ill.mk
+++ ill.mk
@@ -3,8 +3,8 @@ $(call inherit-product, device/htc/m8/full_m8.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/illusion/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/illusion/config/common.mk)
+# phone
+$(call inherit-product, vendor/illusion/config/common_full_phone.mk)
 
 PRODUCT_NAME := ill_m8
 
