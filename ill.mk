639bf59052d02adb4b2b9d0d74490cfcbd48420b
Jake Weinstein <xboxlover360@gmail.com>
2014-05-26 19:46:50 -0500

    m8: add illusion boot animation

--- ill.mk
+++ ill.mk
@@ -7,3 +7,11 @@ $(call inherit-product, vendor/illusion/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/illusion/config/common.mk)
 
 PRODUCT_NAME := ill_m8
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# bootanimation
+PRODUCT_COPY_FILES += \
+        vendor/illusion/bootanimation/1080x1920.zip:system/media/bootanimation.zip
